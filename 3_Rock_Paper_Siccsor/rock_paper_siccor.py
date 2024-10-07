da5e4f9283eb6da06c5aa06d8777ed3ea05b5361
Manikandan1818 <simbumanii18@gmail.com>
2024-10-07 22:28:08 +0530

    Rock and paper game coding completed

--- 3_Rock_Paper_Siccsor/rock_paper_siccor.py
+++ 3_Rock_Paper_Siccsor/rock_paper_siccor.py
@@ -1,13 +1,51 @@
 import random
 
-rock = "Rock"
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-paper ="Paper"
+paper ='''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-scissor ="Scissor"
+scissor ='''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 
+
+game_Images = [rock, paper, scissor]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper and 2 for scissors.\n"))
+print(game_Images[user_choice])
 computer_choice = random.randint(0, 2)
-print(f"Computer Chose: {computer_choice}")
+print("Computer Chose")
+print(game_Images[computer_choice])
 
-if user_choice
+if user_choice > 2 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's draw!")
+else:
+    print("You typed an invalid number. You lose!")
