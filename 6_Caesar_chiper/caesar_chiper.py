aea0a13460dd80feedad53f14afd0e8ab7781103
Manikandan1818 <simbumanii18@gmail.com>
2024-10-15 21:43:49 +0530

    Caeser game completed using while and function

--- 6_Caesar_chiper/caesar_chiper.py
+++ 6_Caesar_chiper/caesar_chiper.py
@@ -1,8 +1,9 @@
+import art
+
+print(art.logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # def encrypt(original_text, shift_amount):
 #     cipher_text = "" #j    
@@ -26,15 +27,30 @@ shift = int(input("Type the shift number:\n"))
 def ceaser(original_text, shift_amount, encode_or_decode):
     output_text = ""
     
+    if encode_or_decode == "decode":
+        shift_amount *= -1               
+               
     for letter in original_text:
 
-        if encode_or_decode == "decode":
-            shift_amount *= -1
-        
-        shifted_position = alphabet.index(letter)  + shift_amount
-        shifted_position %= len(alphabet) #0-25
-        output_text += alphabet[shifted_position] #j    
+        if letter not in alphabet:
+            output_text += letter
+        else:       
+            shifted_position = alphabet.index(letter)  + shift_amount
+            shifted_position %= len(alphabet) #0-25
+            output_text += alphabet[shifted_position] #j    
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
+should_Continue = True
 
-ceaser(original_text=text, shift_amount=shift, encode_or_decode=direction)
+while should_Continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    ceaser(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_Continue = False
+        print("Good Bye")
+
