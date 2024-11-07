47f12515a1b4b894b260113e1002206d8922c179
Manikandan1818 <simbumanii18@gmail.com>
2024-11-07 22:13:01 +0530

    resource sufficiant checking

--- 11_Coffee_Machine/main.py
+++ 11_Coffee_Machine/main.py
@@ -23,9 +23,34 @@ MENU = {
         "cost": 3.0,
     }
 }
-
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+def is_resource_sufficiant(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+
+is_on = True
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        is_resource_sufficiant(drink['ingredients'])
+
+        
