1b8b7065cc1e8b236eb9471d37786fc05e906e07
Manikandan1818 <simbumanii18@gmail.com>
2024-11-18 22:08:56 +0530

    Quiz project completed by using class attributes and methods

--- 13_Quiz/main.py
+++ 13_Quiz/main.py
@@ -11,4 +11,9 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
