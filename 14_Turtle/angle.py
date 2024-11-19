a712353e55bc4cf5784217512bab17a82292dc08
Manikandan1818 <simbumanii18@gmail.com>
2024-11-19 22:17:19 +0530

    Turtle package installed and created!

---
+++ 14_Turtle/angle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+tim = Turtle()
+tim.speed("slowest")
+
+colors = ["navy", "dark olive green", "slate blue", "medium violet red", "dark red", "dark red", "gold"]
+
+def draw_shape(num_sides):
+    """Get number of sides and color."""
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+        
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
--- 14_Turtle/main.py
+++ 14_Turtle/main.py
@@ -1,19 +1,21 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("arrow")
+timmy_the_turtle.color("dark olive green")
 
+timmy_the_turtle.speed("slowest")
 
+# for _ in range(8):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(45)
 
-
-
-
-
-
-
-
-
-
-
+for _ in range(18):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
+    
 
 
 screen = Screen()
